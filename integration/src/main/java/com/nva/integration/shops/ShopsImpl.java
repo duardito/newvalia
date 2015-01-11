c3b79bd8f641f9a831e4823f0b82ab527982fe4d
duardito <eduard.frades@gmail.com>
2015-01-11 09:58:10 +0100

    #newvalia-9 fixed shop stored in database.

--- integration/src/main/java/com/nva/integration/shops/ShopsImpl.java
+++ integration/src/main/java/com/nva/integration/shops/ShopsImpl.java
@@ -3,9 +3,12 @@ package com.nva.integration.shops;
 import com.nva.service.shops.ShopServiceInterface;
 import com.nva.support.beans.shops.ShopVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by edu on 19/12/2014.
@@ -18,8 +21,16 @@ public class ShopsImpl implements ShopsInterface{
 
     @RequestMapping(value = "/shop/save", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
     @Override
-    public ShopVO save(ShopVO shopVO) {
+    public ShopVO save(@RequestBody ShopVO shopVO) {
 
         return shopServiceInterface.saveShop(shopVO);
     }
+
+    @RequestMapping(value = "/shop/findAll", method = RequestMethod.GET, produces = "application/json")
+    @Override
+    public List<ShopVO> findAll() {
+        return shopServiceInterface.findAll();
+    }
+
+
 }
--- service/src/main/java/com/nva/service/shops/ShopServiceInterface.java
+++ service/src/main/java/com/nva/service/shops/ShopServiceInterface.java
@@ -2,10 +2,13 @@ package com.nva.service.shops;
 
 import com.nva.support.beans.shops.ShopVO;
 
+import java.util.List;
+
 /**
  * Created by edu on 19/12/2014.
  */
 public interface ShopServiceInterface {
 
     public ShopVO saveShop(final ShopVO shopVO);
+    List<ShopVO> findAll();
 }
